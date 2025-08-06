b277356b8273f8a25a0c466b77efa5a27004627d
Germano <germano12344@gmail.com>
2025-08-06 20:32:13 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/astentask/AstentaskApplication.java
@@ -0,0 +1,13 @@
+package com.astentask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstentaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstentaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astentask/AstentaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astentask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstentaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
