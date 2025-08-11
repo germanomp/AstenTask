1e4b76b1c16d104df02d9802f7d6748aec1d12b1
Germano <germano12344@gmail.com>
2025-08-11 14:11:52 -0300

    feat: Classe de configuração Open Api Swagger

---
+++ src/main/java/com/astentask/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.astentask.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("AstenTask")
+                        .version("1.0")
+                        .description("Documentação da API de gerenciamento de projetos e tarefas - AstenTask")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+
+}
